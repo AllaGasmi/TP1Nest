dccc0d9c9d2e53e629b927386710ffcb3d9abd97
Alla Gasmi <alla.gasmi@gmail.insat.ucar.tn>
2026-04-01 15:43:57 +0100

    ajout des entités + seed de la base

---
+++ src/commands/cv-seeder.ts
@@ -0,0 +1,53 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../app.module';
+import { CvService } from '../cv/cv.service';
+import { UserService } from '../user/user.service';
+import { SkillService } from '../skill/skill.service';
+import {randFirstName,randLastName,randJobTitle,randNumber,randUuid,} from '@ngneat/falso';
+import { Skill } from '../skill/entities/skill.entity';
+import { User } from '../user/entities/user.entity';
+
+async function bootstrap() {
+const app = await NestFactory.createApplicationContext(AppModule);
+  const cvService = app.get(CvService);
+  const userService = app.get(UserService);
+  const skillService = app.get(SkillService);
+
+  const users: User[] = [];
+  for (let i = 0; i < 5; i++) {
+    const user = await userService.create({
+      username: randFirstName(),
+      email: `${randFirstName()}@mail.com`,
+      password: 'password123',
+    });
+    // users.push(user);
+  }
+
+  const skills: Skill[] = [];
+  for (let i = 0; i < 10; i++) {
+    const skill = await skillService.create({
+      designation: randJobTitle(),
+    });
+    // skills.push(skill);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const randomUser = users[randNumber({ min: 0, max: users.length - 1 })];
+    const randomSkillId = randNumber({ min: 0, max: skills.length - 1 });
+    await cvService.create(
+      {
+        name: randLastName(),
+        firstname: randFirstName(),
+        age: randNumber({ min: 20, max: 50 }),
+        Cin: randUuid(),
+        Job: randJobTitle(),
+        path: 'path/to/file.pdf',
+        skillIds: [randomSkillId],
+      },
+    );
+    }
+    
+  await app.close();
+}
+
+bootstrap();
---
+++ src/cv/entities/cv.entity.ts
@@ -0,0 +1,34 @@
+import { Skill } from 'src/skill/entities/skill.entity';
+import { User } from 'src/user/entities/user.entity';
+import {Entity,PrimaryGeneratedColumn,Column,ManyToOne,ManyToMany,JoinTable} from 'typeorm';
+@Entity()
+export class Cv {
+  @PrimaryGeneratedColumn()
+    id: number;
+    
+  @Column()
+    name: string;
+    
+  @Column()
+    firstname: string;
+    
+  @Column()
+    age: number;
+    
+  @Column()
+    Cin: string;
+    
+  @Column()
+    Job: string;
+    
+  @Column()
+    path: string;
+    
+  @ManyToOne(() => User, (user) => user.cvs, { onDelete: 'CASCADE' })
+    user: User;
+    
+  @ManyToMany(() => Skill, { onDelete: 'CASCADE' })
+  @JoinTable()
+    skills: Skill[];
+    
+}
